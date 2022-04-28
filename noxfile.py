0f8e044cac2ceca93df8899fa4581f4155f97d72
lWarne <laurencewarne@gmail.com>
2022-04-28 11:08:28 +0100

    Integrate coverage

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ from nox_poetry import session
 package = "cfn_lsp_extra"
 python_versions = ["3.10", "3.9", "3.8", "3.7"]
 nox.needs_version = ">= 2021.6.6"
-nox.options.sessions = ("tests", "lint", "mypy")
+nox.options.sessions = ("tests", "lint", "mypy", "coverage")
 
 
 @session(python=python_versions)
@@ -28,3 +28,13 @@ def mypy(session):
     session.install(".")
     session.install("mypy")
     session.run("mypy", package)
+
+
+@session(python=python_versions)
+def coverage(session):
+    session.install("pytest", ".")
+    session.install("pytest-asyncio", ".")
+    session.install("pytest-mock", ".")
+    session.install("coverage", ".")
+    session.run("coverage", "run", "-m", "pytest")
+    session.run("coverage", "xml")
