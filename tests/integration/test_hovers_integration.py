5fbcc7301e2030817521a6158f10b8d42b6b0210
lWarne <laurencewarne@gmail.com>
2022-06-21 14:27:00 +0100

    Use lsp-pytest client method hover_request which *does* exist

--- tests/integration/test_hovers_integration.py
+++ tests/integration/test_hovers_integration.py
@@ -17,37 +17,28 @@ pytestmark = pytest.mark.integration
 @pytest.mark.asyncio
 async def test_property_hover(client):
     test_uri = client.root_uri + "/template.yaml"
-    result = await client.lsp.send_request_async(
-        HOVER,
-        HoverParams(
-            text_document=TextDocumentIdentifier(uri=test_uri),
-            position=Position(line=57, character=8),
-        ),
+    result = await client.hover_request(
+        uri=test_uri,
+        position=Position(line=57, character=8),
     )
-    assert "Bucket" in Hover(**result).contents.value
+    assert "Bucket" in result.contents.value
 
 
 @pytest.mark.asyncio
 async def test_resource_hover(client):
     test_uri = client.root_uri + "/template.yaml"
-    result = await client.lsp.send_request_async(
-        HOVER,
-        HoverParams(
-            text_document=TextDocumentIdentifier(uri=test_uri),
-            position=Position(line=54, character=10),
-        ),
+    result = await client.hover_request(
+        uri=test_uri,
+        position=Position(line=54, character=10),
     )
-    assert "AWS::S3::BucketPolicy" in Hover(**result).contents.value
+    assert "AWS::S3::BucketPolicy" in result.contents.value
 
 
 @pytest.mark.asyncio
 async def test_nested_property_hover(client):
     test_uri = client.root_uri + "/template.yaml"
-    result = await client.lsp.send_request_async(
-        HOVER,
-        HoverParams(
-            text_document=TextDocumentIdentifier(uri=test_uri),
-            position=Position(line=47, character=8),
-        ),
+    result = await client.hover_request(
+        uri=test_uri,
+        position=Position(line=47, character=8),
     )
-    assert "DestinationBucketName" in Hover(**result).contents.value
+    assert "DestinationBucketName" in result.contents.value
