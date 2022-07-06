e57e9f1ad28d76ec4be4dc4693733afcf2f63678
lWarne <laurencewarne@gmail.com>
2022-07-06 11:23:41 +0100

    Use TextEdit for ref completions

--- cfn_lsp_extra/completions/__init__.py
+++ cfn_lsp_extra/completions/__init__.py
@@ -41,7 +41,7 @@ def completions_for(
     prop_span = prop_lookup.at(line, char)
     if prop_span:
         return property_completions(prop_span.value, aws_context, document, position)
-    ref_completions_result = ref_completions(template_data, position)
+    ref_completions_result = ref_completions(template_data, document, position)
     if ref_completions_result:
         return ref_completions_result
     return intrinsic_function_completions(document, position)
@@ -78,6 +78,7 @@ def property_completions(
 
 def ref_completions(
     template_data: Tree,
+    document: Document,
     position: Position,
     ref_extractor: Extractor[AWSRefName] = REF_EXTRACTOR,
     ref_src_extractor: Extractor[AWSRefSource] = REF_SRC_EXTRACTOR,
@@ -85,11 +86,13 @@ def ref_completions(
     ref_lookup = ref_extractor.extract(template_data)
     ref_span = ref_lookup.at(position.line, position.character)
     if ref_span:
+        before, after = word_before_after_position(document.lines, position)
         ref_src_lookup = ref_src_extractor.extract(template_data)
         items = [
             CompletionItem(
                 label=ref_src.logical_name,
                 documentation=ref_src.as_documentation(),
+                text_edit=text_edit(position, before, after, ref_src.logical_name),
             )
             for ref_src, _ in ref_src_lookup.items()
         ]
