8e80c6daa0aaf41e717b3619c01c14c62138a194
lWarne <laurencewarne@gmail.com>
2022-07-09 10:21:36 +0100

    Fix informational and warning diagnostics shown as errors

--- cfn_lsp_extra/cfnlint_integration.py
+++ cfn_lsp_extra/cfnlint_integration.py
@@ -12,6 +12,8 @@ import cfnlint.config
 import cfnlint.core
 import cfnlint.decode
 import cfnlint.runner
+from cfnlint.core import get_rules
+from cfnlint.helpers import REGIONS
 from cfnlint.rules import ParseError
 from cfnlint.rules import RulesCollection
 from cfnlint.rules import TransformError
@@ -24,19 +26,19 @@ from pygls.lsp.types import Range
 def diagnostics(yaml_content: str, file_path: str) -> List[Diagnostic]:
     """Return diagnostics for the template file at file_path."""
     cfnlint.config.configure_logging(None, None)
-    conf, _, _ = cfnlint.core.get_args_filenames([])
-    template, rules, errors = _get_template_rules(yaml_content, file_path, conf)
+    rules = get_rules([], [], ["I", "W", "E"], include_experimental=True)
+    template, errors = _decode(yaml_content, file_path)
 
     if not errors:
         runner = cfnlint.runner.Runner(
-            rules, file_path, template, regions=conf.regions, mandatory_rules=None
+            rules, file_path, template, regions=REGIONS, mandatory_rules=None
         )
         errors: List[cfnlint.rules.Match] = runner.run()
 
     severity_mapping: Dict[str, DiagnosticSeverity] = {
-        "INFORMATIONAL": DiagnosticSeverity.Information,
-        "ERROR": DiagnosticSeverity.Error,
-        "WARNING": DiagnosticSeverity.Warning,
+        "informational": DiagnosticSeverity.Information,
+        "error": DiagnosticSeverity.Error,
+        "warning": DiagnosticSeverity.Warning,
     }
     return [
         Diagnostic(
@@ -46,16 +48,17 @@ def diagnostics(yaml_content: str, file_path: str) -> List[Diagnostic]:
             ),
             message=m.message,
             source="cfn-lsp-extra",
-            severity=severity_mapping.get(m.rule.severity, DiagnosticSeverity.Error),
+            severity=severity_mapping.get(
+                m.rule.severity.lower(), DiagnosticSeverity.Error
+            ),
         )
         for m in errors
     ]
 
 
 """
-We make alterations here to `cfnlint.core.get_template_rules` and
-`cfnlint.decode.decode` so they work on (yaml) strings rather than files.
-This is a bit
+We make alterations here to `cfnlint.decode.decode` so it works on (yaml)
+strings rather than files.  This is a bit
 
 ⣶⣶⣶⣦⠀⠀⠀⣰⣷⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣾⣆⠀⠀⠀⣴⣶⣶⣶
 ⠛⠻⣿⣿⡀⠀⢠⣿⣿⠏⠀⠀⢀⠀⢤⣴⣆⠀⠀⠀⠹⣿⣿⡄⠀⢀⣿⣿⠟⠛
@@ -68,45 +71,6 @@ This is a bit
 An alternative would be to create our own interpreter for the set of rules
 from `cfnlint.core.get_rules`.
 """
-
-
-def _get_template_rules(yaml_content, filename, args):
-    """Get Template Configuration items and set them as default values"""
-
-    ignore_bad_template = False
-    if args.ignore_bad_template:
-        ignore_bad_template = True
-    else:
-        # There is no collection at this point so we need to handle this
-        # check directly
-        if not ParseError().is_enabled(
-            include_experimental=False,
-            ignore_rules=args.ignore_checks,
-            include_rules=args.include_checks,
-            mandatory_rules=args.mandatory_checks,
-        ):
-            ignore_bad_template = True
-
-    (template, errors) = _decode(yaml_content, filename)
-
-    if errors:
-        if len(errors) == 1 and ignore_bad_template and errors[0].rule.id == "E0000":
-            return (template, [], [])
-        return (template, [], errors)
-
-    args.template_args = template
-
-    rules = cfnlint.core.get_rules(
-        args.append_rules,
-        args.ignore_checks,
-        args.include_checks,
-        args.configure_rules,
-        args.include_experimental,
-        args.mandatory_checks,
-        args.custom_rules,
-    )
-
-    return (template, rules, [])
 
 
 import json
