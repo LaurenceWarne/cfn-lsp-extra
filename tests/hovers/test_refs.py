fb15c11d733dc8effcdcd56de6975c6948d421d4
Laurence Warne <laurencewarne@gmail.com>
2024-08-05 18:24:49 +0100

    Revert "Reduce duplication in ref tests"
    This reverts commit 4059ab9e5c19adf41ac2f61f2131b74b9f632e74.

--- tests/hovers/test_refs.py
+++ tests/hovers/test_refs.py
@@ -2,11 +2,12 @@
 Tests for ref hovers.
 """
 import pytest
+from lsprotocol.types import Position
+from pygls.workspace import Document
+
 from cfn_lsp_extra.aws_data import AWSResourceName
 from cfn_lsp_extra.decode import decode
 from cfn_lsp_extra.hovers.refs import ref_hover
-from lsprotocol.types import Position
-from pygls.workspace import Document
 
 from ..test_aws_data import full_aws_context
 
@@ -51,20 +52,16 @@ def document(document_string):
     return Document(uri="", source=document_string)
 
 
-@pytest.fixture
-def document_tree(document):
-    return decode(document_string, "file.yaml")
-
-
 def test_ref_resource_hover(
     full_aws_context,
-    document_tree,
+    document_string,
     document,
 ):
     line, char = 25, 18
+    tree = decode(document_string, "file.yaml")
 
     result = ref_hover(
-        document_tree,
+        tree,
         Position(line=line, character=char),
         full_aws_context,
         document,
@@ -75,13 +72,14 @@ def test_ref_resource_hover(
 
 def test_parameter_resource_hover(
     full_aws_context,
-    document_tree,
+    document_string,
     document,
 ):
     line, char = 17, 25
+    tree = decode(document_string, "file.yaml")
 
     result = ref_hover(
-        document_tree,
+        tree,
         Position(line=line, character=char),
         full_aws_context,
         document,
@@ -93,10 +91,12 @@ def test_parameter_resource_hover(
 def test_no_ref_hover(
     full_aws_context,
     document_string,
-    document_tree,
+    document,
 ):
+    tree = decode(document_string, "file.yaml")
+
     result = ref_hover(
-        document_tree,
+        tree,
         Position(line=42, character=8),
         full_aws_context,
         document,
