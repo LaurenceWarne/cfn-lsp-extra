b8139573454e6578c751a1773a96a629d21f3c02
Laurence Warne <laurencewarne@gmail.com>
2024-11-23 17:11:08 +0000

    Update noxfile versions

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,8 @@
 import nox
 
 package = "cfn_lsp_extra"
-python_versions = ["3.12", "3.11", "3.10", "3.9", "3.8"]
-nox.needs_version = ">= 2021.6.6"
+python_versions = ["3.13", "3.12", "3.11", "3.10", "3.9", "3.8"]
+nox.needs_version = ">= 2024.10.9"
 nox.options.sessions = ("tests", "integration_tests", "lint", "mypy", "coverage")
 
 
