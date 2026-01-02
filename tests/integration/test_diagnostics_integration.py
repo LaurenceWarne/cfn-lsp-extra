5d4c987f1575e894eb199cdc2ed83017670eea12
Laurence Warne <laurencewarne@gmail.com>
2026-01-02 15:52:45 +0000

    Make diagnostics integration tests less flakey

--- tests/integration/test_diagnostics_integration.py
+++ tests/integration/test_diagnostics_integration.py
@@ -20,8 +20,9 @@ pytestmark = pytest.mark.integration
 @pytest.mark.parametrize(
     "file_name,diagnostics",
     [
-        ("template.yaml", ["'OriginAccessIdentity' dependency already enforced by a 'GetAtt' at 'Resources/WebBucketPolicy/Properties/PolicyDocument/Statement/0/Principal/CanonicalUser'", "'OriginAccessIdentity' dependency already enforced by a 'Ref' at 'Resources/Distribution/Properties/DistributionConfig/Origins/0/S3OriginConfig/OriginAccessIdentity/Fn::Join/1/2'", "'Suspe' is not one of ['Enabled', 'Suspended']", "'AccessControl' is a legacy property. Consider using 'AWS::S3::BucketPolicy' instead", "A bucket with 'AccessControl' set should also have at least one 'OwnershipControl' configured", "'Type' is a required property", "None is not of type 'string'", "'A' is not one of ['Arn', 'RoleId'] in ['us-east-1']"]),
-        ("template.json", ["'OriginAccessIdentity' dependency already enforced by a 'GetAtt' at 'Resources/WebBucketPolicy/Properties/PolicyDocument/Statement/0/Principal/CanonicalUser'", "'OriginAccessIdentity' dependency already enforced by a 'Ref' at 'Resources/Distribution/Properties/DistributionConfig/Origins/0/S3OriginConfig/OriginAccessIdentity/Fn::Join/1/2'", "'Suspend' is not one of ['Enabled', 'Suspended']", "'AccessControl' is a legacy property. Consider using 'AWS::S3::BucketPolicy' instead", "A bucket with 'AccessControl' set should also have at least one 'OwnershipControl' configured", "Resource type '' does not exist in 'us-east-1'"]),
+        ("template.yaml", {("'OriginAccessIdentity' dependency already enforced by a 'GetAtt' at 'Resources/WebBucketPolicy/Properties/PolicyDocument/Statement/0/Principal/CanonicalUser'", (55, 4), (55, 13)), ("'OriginAccessIdentity' dependency already enforced by a 'Ref' at 'Resources/Distribution/Properties/DistributionConfig/Origins/0/S3OriginConfig/OriginAccessIdentity/Fn::Join/1/2'", (74, 4), (74, 13)), ("'Suspe' is not one of ['Enabled', 'Suspended']", (135, 8), (135, 14)), ("'AccessControl' is a legacy property. Consider using 'AWS::S3::BucketPolicy' instead", (133, 6), (133, 19)), ("A bucket with 'AccessControl' set should also have at least one 'OwnershipControl' configured", (132, 4), (132, 14)), ("'Type' is a required property", (148, 2), (148, 20)), ("None is not of type 'string'", (149, 4), (149, 8)), ("'A' is not one of ['Arn', 'RoleId'] in ['us-east-1']", (172, 6), (172, 22))}),
+        ("template.json", {("'OriginAccessIdentity' dependency already enforced by a 'GetAtt' at 'Resources/WebBucketPolicy/Properties/PolicyDocument/Statement/0/Principal/CanonicalUser'", (67, 12), (67, 23)), ("'OriginAccessIdentity' dependency already enforced by a 'Ref' at 'Resources/Distribution/Properties/DistributionConfig/Origins/0/S3OriginConfig/OriginAccessIdentity/Fn::Join/1/2'", (109, 12), (109, 23)), ("'Suspend' is not one of ['Enabled', 'Suspended']", (244, 20), (244, 28)), ("'AccessControl' is a legacy property. Consider using 'AWS::S3::BucketPolicy' instead", (242, 16), (242, 31)), ("A bucket with 'AccessControl' set should also have at least one 'OwnershipControl' configured", (241, 12), (241, 24)), ("Resource type '' does not exist in 'us-east-1'", (275, 12), (275, 18))
+}),
     ],
 )
 @pytest.mark.asyncio
@@ -44,6 +45,5 @@ async def test_diagnostics(client: LanguageClient, file_name, diagnostics):
     await client.wait_for_notification(TEXT_DOCUMENT_PUBLISH_DIAGNOSTICS)
 
     assert uri in client.diagnostics
-
-    diags = [m.message for m in client.diagnostics[uri]]
-    assert diags == diagnostics
+    diags = [(m.message, (m.range.start.line, m.range.start.character), (m.range.end.line, m.range.end.character)) for m in client.diagnostics[uri]]
+    assert set(diags) == diagnostics
